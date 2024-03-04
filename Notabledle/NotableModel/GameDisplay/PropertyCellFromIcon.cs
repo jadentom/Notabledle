d459388eb22e8b8801f90f22ea4aeae494266b78
Jaden Tom <jadenttom@gmail.com>
2024-03-03 21:47:15 -0500

    Forgot to add cs

---
+++ Notabledle/NotableModel/GameDisplay/PropertyCellFromIcon.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace Notabledle.NotableModel.GameDisplay
+{
+    public class PropertyCellFromIcon : IPropertyCellFromProperty
+    {
+        public string HeaderName => "Icon";
+
+        public string HelpText => "The notable icon.";
+
+        public PropertyCell PropertyCellFromProperty(Notable guessGem, Notable targetGem, int i) =>
+            new PropertyCell($"images/notable_{guessGem.Id}.webp");
+    }
+}
